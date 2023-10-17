f4c747e11156779ed4f8fbbb3d62040440fef0e6
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-10-16 22:14:47 -0300

    feat: physical exercise model and migration

---
+++ src/database/migrations/20231017000451-create-physical-exercise.js
@@ -0,0 +1,67 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("PhysicalExercise", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      exerciseName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [5, 100],
+        },
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      time: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      exerciseType: {
+        type: Sequelize.ENUM(
+          "aerobic endurance",
+          "muscular endurance",
+          "flexibility",
+          "strength",
+          "agility",
+          "other"
+        ),
+        allowNull: false,
+      },
+      quantityPerWeek: {
+        type: Sequelize.FLOAT(2, 2),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [10, 1000],
+        },
+      },
+      systemStatus: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("PhysicalExercise");
+  },
+};
