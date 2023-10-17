b6fde666a85ab5980171f7ebd318335b92af7a46
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-10-17 11:50:36 -0300

    [BACK-010] Criação do Modelo de Log.

---
+++ src/database/migrations/20231017144215-create-log.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Logs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      endpoint: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      httpVerb: {
+        type: Sequelize.ENUM("get", "post", "put", "delete"),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Logs');
+  }
+};
