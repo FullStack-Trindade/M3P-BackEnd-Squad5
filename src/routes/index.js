5bbdaa5ca1cf5023b968faa1f1fc21d4b58f2843
Glauton Osório <glautonleao@gmail.com>
2023-10-18 22:03:01 -0300

    fix: Rota arrumada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const UserRouter = require("./v1/user.routes");
-const AppointmentRouter = require("./v2/appointment.routes");
+const AppointmentRouter = require("./v1/appointment.routes");
 
 router.use("/api", UserRouter);
 router.use("/api", AppointmentRouter);
