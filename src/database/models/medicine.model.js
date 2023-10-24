471ab1f228807c183604b0345aa4355f50cb5fd1
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-10-24 09:04:52 -0300

    [BACK-034]FIX Ajustando configurações do model Medicine

--- src/database/models/medicine.model.js
+++ src/database/models/medicine.model.js
@@ -1,32 +1,28 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
-  class Medicine extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {}
+class Medicine extends Model {
+  static associate(models) {}
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        date: DataTypes.DATEONLY,
+        time: DataTypes.TIME,
+        type: DataTypes.ENUM,
+        amount: DataTypes.FLOAT,
+        unit: DataTypes.ENUM,
+        comments: DataTypes.STRING,
+        systemStatus: DataTypes.BOOLEAN,
+        patientId: DataTypes.INTEGER,
+        userId: DataTypes.INTEGER,
+      },
+      {
+        sequelize,
+        modelName: "Medicine",
+      }
+    );
   }
-  Medicine.init(
-    {
-      name: DataTypes.STRING,
-      date: DataTypes.DATEONLY,
-      time: DataTypes.TIME,
-      type: DataTypes.ENUM,
-      amount: DataTypes.FLOAT,
-      unit: DataTypes.ENUM,
-      comments: DataTypes.STRING,
-      systemStatus: DataTypes.BOOLEAN,
-      patientId: DataTypes.INTEGER,
-      userId: DataTypes.INTEGER,
-    },
-    {
-      sequelize,
-      modelName: "Medicine",
-    }
-  );
-  return Medicine;
-};
+}
+
+module.exports = Medicine;
