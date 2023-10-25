0e8fb8feba1728d945cd8184bb47706fdfd7bd75
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-10-24 23:19:46 -0300

    feat: exam routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const UserRouter = require("./v1/user.routes");
 const AppointmentRouter = require("./v1/appointment.routes");
+const ExamRouter = require("./v1/exam.routes");
 
 router.use("/api", UserRouter);
 router.use("/api", AppointmentRouter);
+router.use("/api", ExamRouter);
 
 module.exports = router;
