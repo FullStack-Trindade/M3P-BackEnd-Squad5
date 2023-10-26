0fdf10a9dcffec401bf73af7104f6716414652cd
Glauton Osório <glautonleao@gmail.com>
2023-10-26 05:50:22 -0300

    feat: Nova validação feita

--- src/controllers/DietController/functions/diet.index.js
+++ src/controllers/DietController/functions/diet.index.js
@@ -3,19 +3,32 @@ const Diet = require("../../../database/models/diet.model");
 module.exports.findAllDiet = async (req, res) => {
   try {
     const {
+      params: { id },
       query: { patientId },
     } = req;
+    if ((patientId && isNaN(patientId)) || (id && isNaN(id))) {
+      const err = new Error("patientId deve ser um INTEGER");
+      err.status = 400;
+      throw err;
+    }
+    if (id) {
+      const diet = await Diet.findByPk(id);
+      if (!diet) {
+        const err = new Error("Dieta não encontrado");
+        return res.status(400).send(` message: ${err} `);
+      }
+      return res.status(200).send({ data: diet });
+    }
     if (patientId) {
       const diet = await Diet.findAll({
         where: { patientId: patientId },
       });
-      return res.status(200).send({ data: diet });
-    } else {
-      const diet = await Diet.findAll();
+
       return res.status(200).send({ data: diet });
     }
+    const diet = await Diet.findAll();
+    return res.status(200).send({ data: diet });
   } catch (err) {
-    console.log(err);
-    return res.status(500).send({ err: err.message });
+    return res.status(err.status || 500).send({ err: err.message });
   }
 };
