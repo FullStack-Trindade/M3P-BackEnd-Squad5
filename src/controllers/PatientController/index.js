e090afb64a0e0523448f815c41020aa361aa471f
icarofilho <icaro.ferreira.filho@gmail.com>
2023-10-28 16:44:36 -0300

    doc: adicionado documentação no swagger

--- src/controllers/PatientController/index.js
+++ src/controllers/PatientController/index.js
@@ -2,6 +2,62 @@ const { createNewPatient } = require("./function/patient.store");
 
 class PatientController {
   async store(req, res) {
+    // #swagger.tags = ['Patients']
+    // #swagger.summary = 'Cadastra um novo paciente.'
+    // #swagger.description = 'Endpoint para cadastrar um novo paciente e endereço'
+    /* #swagger.parameters["body"] = { 
+        in: "body",
+        description:"
+        <u>
+          <li><b>fullName</b>: Nome do paciente com máximo e mínimo de 64 e 8 caracteres, respectivamente.<mark>Campo obrigatório</mark></li>
+          <li><b>gender</b>: Genero do paciente, com opções pré-definidas: male, female, other. <mark>Campo obrigatório</mark></li>
+          <li><b>birthday</b>: Data de nascimento do paciente, no formato YYYY-MM-DD.<mark>Campo obrigatório</mark></li>
+          <li><b>cpf</b>: CPF do paciente no formato 000.000.000-00 <mark>Campo obrigatório</mark></li>
+          <li><b>RG</b>: RG do paciente, deve conter no máximo 20 caracteres. <mark>Campo obrigatório</mark></li>
+          <li><b>civilStatus</b>: Estado civil do paciente, deve contemplar um dos seguintes valores: single, married, divorced, widowed, separated. <mark>Campo obrigatório</mark></span></li>
+          <li><b>phoneNumber</b>: Telefone do usuário, deve ser informado apenas os números e deve possui o DDD: 21988887777. <mark>Campo obrigatório</mark></li>
+          <li><b>email</b>: E-mail utilizado pelo paciente. <mark>Campo obrigatório</mark></li>
+          <li><b>nationality</b>: Nacionalidade do paciente. Campo deve ter de 8 a 64 caracteres <mark>Campo obrigatório</mark></li>
+          <li><b>emergencyContact</b>: Contato de emergencia do paciente, deve ser informado apenas os números e deve possui o DDD: 21988887777. <mark>Campo obrigatório</mark></li>
+          <li><b>listOfAllergies</b>: Lista de alergias do paciente.</li>
+          <li><b>specificCare</b>: Cuidados especiais do paciente.</li>
+          <li><b>healthInsurance</b>: Convênio do paciente.</li>
+          <li><b>insuranceNumber</b>: Número do convênio do paciente.</li>
+          <li><b>insuranceExpirationDate</b>: Validade do convênio do paciente. Deve estar no formato  YYYY-MM-DD</li>
+          <li><b>systemStatus</b>: Status no sistema, sendo sendo um boolean com valor TRUE. OBS: caso não informado, <mark>será cadastrado como TRUE(Ativo)</mark></li>
+          <li><b>userId</b>: ID do usuário relacionado. OBS: Não pode ter dois pacientes apontando para o mesmo ID do usuário, <mark>Campo obrigatório</mark></li>
+          <li><b>address</b>: Objeto com os dados do endereço do paciente, sendo eles:</li>
+          <ul>
+            <li>zipCode: CEP do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>city: Cidade do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>state: Estado do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>street: Logradouro do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>number: Numero do logradouro do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>complement: Complemento do logradouro, caso exista.</li>
+            <li>neighborhood: Bairro do paciente.<mark>Campo Obrigatório</mark></li>
+            <li>referencePoint: Ponto de referencia</li>
+          </ul>
+        </u>",
+        type: "object",
+        schema: { $ref: "#/definitions/patientStoreBody" },
+        required: true} */
+    /* #swagger.responses[201] = { 
+          description: 'Exemplo de resposta de sucesso',
+          schema: { $ref: "#/definitions/patientStore201" }
+        } */
+    /* #swagger.responses[400] = { 
+          description: 'Valida os campos e trás o motivo da falha.\n Exemplo: informando estado civil não listado',
+          schema: { $ref: "#/definitions/patientStore400" }
+        } */
+    /* #swagger.responses[409] = { 
+          description: 'Erro de duplicidade\nExemplo: Referenciando um userId já referenciado.',
+          schema: { $ref: "#/definitions/patientStore409" }
+        } */
+    /* #swagger.responses[500] = { 
+          description: 'Caso de erro ao gerar a resposta.\nExemplo: Tentando atribuir um novo valor a uma constante.',
+          schema: { $ref: "#/definitions/userStore500" }
+        } */
+
     await createNewPatient(req, res);
   }
 }
