85831b489e650a5d877d09ead24cc0241f110128
Glauton Osório <glautonleao@gmail.com>
2023-10-30 05:01:27 -0300

    feat: Criando rota de criar exercicios

---
+++ src/routes/v1/physicalExercise.routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const {
+  validateData: PhysicalExerciseCreateValidator,
+} = require("../../middlewares/yupValidate/PhysicalExercise/PhysicalExercise.middleware");
+
+const { authVerify } = require("../../middlewares/auth/auth.middleware");
+
+const {
+  nurse: PermissionValidator,
+} = require("../../middlewares/auth/permissions.middleware");
+const PhysicalExerciseController = require("../../controllers/PhysicalExerciseController");
+
+router.post(
+  "/exercicios",
+  authVerify,
+  PermissionValidator,
+  PhysicalExerciseController.store
+);
