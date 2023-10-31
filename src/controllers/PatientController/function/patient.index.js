85b3329b147986bfc5818c2934ce07f6c2cada2f
icarofilho <icaro.ferreira.filho@gmail.com>
2023-10-31 20:01:53 -0300

    feat: [BACK-052]Adicionado filtro por query

--- src/controllers/PatientController/function/patient.index.js
+++ src/controllers/PatientController/function/patient.index.js
@@ -2,29 +2,56 @@ const connection = require("../../../database");
 
 const Patient = require("../../../database/models/patient.model");
 const Address = require("../../../database/models/address.model");
-const {log} = require("../../../services/logger");
+const { log } = require("../../../services/logger");
 
 module.exports.findAllPatients = async (req, res) => {
   try {
-    const patients = await Patient.findAll({
-      attributes: { exclude: ["createdAt", "updatedAt"] },
-      include: {
-        model: Address,
-        attributes: [
-          "zipCode",
-          "city",
-          "state",
-          "street",
-          "number",
-          "complement",
-          "neighborhood",
-          "referencePoint",
-        ],
-      },
-    });
+    const {
+      query: { email },
+    } = req;
 
-    await log(res.locals.currentUser, `os pacientes`, req);
-    return res.status(200).send({ data: patients });
+    if (email) {
+      const patients = await Patient.findAll({
+        where: { email },
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: {
+          model: Address,
+          attributes: [
+            "zipCode",
+            "city",
+            "state",
+            "street",
+            "number",
+            "complement",
+            "neighborhood",
+            "referencePoint",
+          ],
+        },
+      });
+
+      await log(res.locals.currentUser, `os pacientes`, req);
+      return res.status(200).send({ data: patients });
+    } else {
+      const patients = await Patient.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: {
+          model: Address,
+          attributes: [
+            "zipCode",
+            "city",
+            "state",
+            "street",
+            "number",
+            "complement",
+            "neighborhood",
+            "referencePoint",
+          ],
+        },
+      });
+
+      await log(res.locals.currentUser, `os pacientes`, req);
+      return res.status(200).send({ data: patients });
+    }
   } catch (error) {
     return res
       .status(error.code || error.status || 500)
