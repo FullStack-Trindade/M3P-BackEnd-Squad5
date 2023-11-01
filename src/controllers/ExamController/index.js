e1faa753827a59dd305d90f9e4eff0c1d4c56c60
Glauton Osório <glautonleao@gmail.com>
2023-11-01 15:10:54 -0300

    feat: Swagger de atualização de exame feito

--- src/controllers/ExamController/index.js
+++ src/controllers/ExamController/index.js
@@ -1,5 +1,7 @@
+const { deleteExam } = require("./functions/exam.destroy");
 const { findAllExam } = require("./functions/exam.index");
 const { createNewExam } = require("./functions/exam.store");
+const { updateExam } = require("./functions/exam.update");
 
 class ExamController {
   async index(req, res) {
@@ -65,6 +67,46 @@ class ExamController {
           description: 'Exemplo de resposta de quando não for localizado um exame com o Id fornecido',
           schema: { $ref: "#/definitions/examDestroy400" }
         } */
+    await deleteExam(req, res);
+  }
+  async update(req, res) {
+    // #swagger.tags = ['Exams']
+    // #swagger.summary = 'Atualiza uma exama'
+    // #swagger.description = 'Endpoint para atualizar um exame por meio de seu Id.'
+    // #swagger.parameters['id'] = {in: 'path', type: 'integer', description: 'Exam ID.'}
+
+    /* #swagger.parameters["body"] = { 
+          in: "body",
+        description:"
+        <ul>
+          <li><b>name</b>: Nome do exame com máximo e mínimo de 64 e 8 caracteres, respectivamente.<mark>Campo obrigatório</mark></li>
+          <li><b>examDate</b>: Data do exame <mark>Campo obrigatório</mark></li>
+          <li><b>examTime</b>:Horário do exame <mark>Campo obrigatório</mark></span></li>
+          <li><b>examType</b>: Tipo do exame com máximo e mínimo de 32 e 4 caracteres, respectivamente.<mark>Campo obrigatório</mark></li>
+          <li><b>laboratory</b>:  Laboratório com máximo e mínimo de 32 e 4 caracteres, respectivamente. <mark>Campo obrigatório</mark></li>
+          <li><b>documentURL</b>: URL do documento. <mark>Campo NÃO obrigatório</mark></li>
+          <li><b>results</b>: Resultado do exame com máximo e mínimo de 1024 e 16 caracteres, respectivamente. <mark>Campo obrigatório</mark></li>
+          <li><b>patientId</b>: O id do paciente <mark>Campo obrigatório</mark></li>
+          <li><b>userId</b>: O id do usuário <mark>Campo obrigatório</mark></li>
+          <li><b>systemStatus</b>: Status no sistema, sendo sendo um boolean com valor TRUE. OBS: caso não informado, será cadastrado como Ativo</li>
+        </ul>",
+            type: "object",
+            schema: { $ref: "#/definitions/examUpdateBody" },
+            required: true} */
+
+    /* #swagger.responses[200] = { 
+              description: 'Exemplo de resposta de sucesso',
+              schema: { $ref: "#/definitions/examUpdate200" }
+            } */
+    /* #swagger.responses[400] = { 
+              description: 'Exemplo de resposta de quando não for localizado o exane com o Id fornecido',
+              schema: { $ref: "#/definitions/examUpdate400" }
+            } */
+    /* #swagger.responses[500] = { 
+              description: 'Caso de erro ao gerar a resposta.\nExemplo: Tentando atribuir um novo valor a uma constante.',
+              schema: { $ref: "#/definitions/examStore500" }
+            } */
+    await updateExam(req, res);
   }
 }
 
