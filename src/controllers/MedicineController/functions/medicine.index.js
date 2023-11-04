15804d3c6b442892228d4d1849aff87d231d630c
Glauton Osório <glautonleao@gmail.com>
2023-11-04 06:36:05 -0300

    feat: Adicionado retorno de id na rota de medicina

--- src/controllers/MedicineController/functions/medicine.index.js
+++ src/controllers/MedicineController/functions/medicine.index.js
@@ -1,26 +1,39 @@
 const Medicine = require("../../../database/models/medicine.model");
-const {log} = require("../../../services/logger");
+const { log } = require("../../../services/logger");
 
 module.exports.findAllMedicine = async (req, res) => {
   try {
     const {
+      params: { id },
       query: { patientId },
     } = req;
-    if ((patientId && isNaN(patientId))) {
-      const err = new Error("patientId deve ser um INTEGER");
+    if ((patientId && isNaN(patientId)) || (id && isNaN(id))) {
+      const err = new Error("Id deve ser um INTEGER");
       err.status = 400;
       throw err;
     }
-    const medicine = await (patientId 
-          ? Medicine.findAll({where: { patientId: patientId }}) 
-          : Medicine.findAll());
+    if (id) {
+      const medicine = await Medicine.findByPk(id);
+      if (!medicine) {
+        const err = new Error("Medicamento não encontrado");
+        return res.status(400).send(` message: ${err} `);
+      }
+      await log(
+        res.locals.currentUser,
+        `o medicamento ${id}`,
+        req,
+        medicine.patientId
+      );
+      return res.status(200).send({ data: medicine });
+    }
+    const medicine = await (patientId
+      ? Medicine.findAll({ where: { patientId: patientId } })
+      : Medicine.findAll());
 
     await log(res.locals.currentUser, `os medicamentos`, req, patientId);
     return res.status(200).send({ data: medicine });
   } catch (err) {
     console.log(err);
     return res.status(err.status || 500).send({ err: err.message });
-  } 
-
+  }
 };
-
